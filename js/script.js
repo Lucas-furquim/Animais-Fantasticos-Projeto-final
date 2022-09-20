f3860c326fd1c266e84d234f95e84bb9163aa8e9
Lucas Furquim <lucasviniciusfurquim@gmail.com>
2022-09-20 19:42:28 -0300

    scroll-suave

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import substituiJs from "./modulos/trocajs.js";
 import addData from "./modulos/dataset.js";
-import scroll from "./modulos/scroll-suave.js";
+import ScrollSuave from "./modulos/scroll-suave.js";
 import anima from "./modulos/anima-scroll.js";
 import Accordion from "./modulos/accordion.js";
 import imagens from "./modulos/imagens.js";
@@ -12,12 +12,14 @@ import numeros from "./modulos/numeros.js";
 import funcionamento from "./modulos/tempo.js";
 import compraBitcoin from "./modulos/bitcoin.js";
 
+const scrollSuave = new ScrollSuave("[data-atributes='js-menu'] a[href^='#']");
+scrollSuave.init();
+
 substituiJs();
 addData();
 imagens();
 Accordion();
 anima();
-scroll();
 ativar();
 toltip();
 dropMenu();
