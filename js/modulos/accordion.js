21e78eb5efdc6834bdd959e1628394e1323a7232
Lucas Furquim <lucasviniciusfurquim@gmail.com>
2022-09-21 22:53:09 -0300

    finalizando accordion

--- js/modulos/accordion.js
+++ js/modulos/accordion.js
@@ -1,22 +1,32 @@
 // Accordion List
-export default function Accordion() {
-  const btn = document.querySelectorAll("[data-atributes='js-faq-lista'] dt");
-  btn[0].classList.add("ativo");
-  btn[0].nextElementSibling.classList.add("ativo");
+export default class Accordion {
+  constructor(lista) {
+    this.accLista = document.querySelectorAll(lista);
+    this.activeClass = "ativo";
+  }
 
-  if (btn.length) {
-    btn.forEach((item) => {
-      item.addEventListener("click", (e) => {
-        const dd = e.currentTarget.nextElementSibling;
-        const at = dd.classList.contains("ativo");
-        if (at) {
-          e.currentTarget.classList.remove("ativo");
-          dd.classList.remove("ativo");
-        } else {
-          e.currentTarget.classList.add("ativo");
-          dd.classList.add("ativo");
-        }
-      });
+  activeAccordion(e) {
+    const dd = e.currentTarget.nextElementSibling;
+    const at = dd.classList.contains("ativo");
+    if (at) {
+      e.currentTarget.classList.remove("ativo");
+      dd.classList.remove("ativo");
+    } else {
+      e.currentTarget.classList.add("ativo");
+      dd.classList.add("ativo");
+    }
+  }
+
+  addAccordion() {
+    this.accLista.forEach((item) => {
+      item.addEventListener("click", this.activeAccordion);
     });
+  }
+
+  init() {
+    this.accLista[0].classList.add(this.activeClass);
+    this.accLista[0].nextElementSibling.classList.add(this.activeClass);
+    this.addAccordion();
+    return this;
   }
 }
